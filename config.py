d4d017d42e112e9f9622ec95706225ce2c8c2ea6
Asilbek <asilbek.sadullayev000@example.com>
2025-07-30 23:57:38 +0500

    endida

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BOT_TOKEN =os.getenv("BOT_TOKEN")  # Telegram bot tokeni
 ADMIN_IDS = [5306481482,7646223205,5902572920,1846386540,5705506626]  # Admin foydalanuvchi ID’lari
 # CHANNEL_IDS = ['2052256946']
 #  # Majburiy obuna kanallari
-CHANNEL_IDS =['2708822689']
+CHANNEL_IDS =['kino_kodlar_t']
 CHANNEL_ID1="https://t.me/KinoBotProPlus"  # Kanalga havola
 DB_PATH = "database/bot.db"  # Ma’lumotlar bazasi fayli
 REFERRAL_BONUS = 5  # Referal uchun bonus chegarasi
