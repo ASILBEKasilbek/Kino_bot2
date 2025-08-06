dd296cb4774eaf96d85dea2656c85ebde752bfb1
Asilbek <asilbek.sadullayev000@example.com>
2025-08-06 12:17:06 +0500

    nima

--- handlers/support.py
+++ handlers/support.py
@@ -6,7 +6,7 @@ support_router = Router()
 @support_router.message(F.text == "/support")
 async def support_command(message: Message):
     await message.answer(
-        "🆘 <b>Texnik yordam kerakmi?</b>\n\n"
-        "Agar sizda savollar, muammolar yoki takliflar bo‘lsa, biz bilan bog‘laning.\n",
+        """🆘 <b>Texnik yordam kerakmi?</b>\n\n
+        Agar sizda savollar, muammolar yoki takliflar bo‘lsa, biz bilan bog‘laning.\n""",
         parse_mode="HTML"
     )
